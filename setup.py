e30b6613edcea59bd789bc0f6fcfb72bce404818
Justin Su <justin.su@enovant.com>
2019-10-10 14:01:06 -0400

    updated app version

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='NLP4J Wrapper',
-      version='0.1',
+      version='0.2',
       description='A python wrapper for the cli tool of NLP4J library',
       url='',
       author='Justin So',
-      author_email='q.justin.so@gmail.com',
+      author_email='justinsowhatnow@gmail.com',
       license='MIT',
       packages=['nlp4j_wrapper'],
       zip_safe=False)
